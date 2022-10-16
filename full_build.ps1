8277e8d4dba486d8f61867a03351009066ef7fb9
cedriclikescode <104719938+cedriclikescode@users.noreply.github.com>
2022-10-16 17:20:45 +0200

    add windows arm64 experimental build

--- full_build.ps1
+++ full_build.ps1
@@ -8,6 +8,7 @@ Catch {
 Finally {
     Write-Host "Delete of Robintris64.exe finished. Building new one."
 }
+
 $Env:GOARCH = 'amd64'
 go build -o Robintris64.exe -tags=ebitenginesinglethread main.go
 Remove-Item Env:GOARCH
@@ -21,6 +22,7 @@ Catch {
 Finally {
     Write-Host "Delete of Robintris32.exe finished. Building new one."
 }
+
 $Env:GOARCH = '386'
 go build -o Robintris32.exe -tags=ebitenginesinglethread main.go
 Remove-Item Env:GOARCH
@@ -34,6 +36,21 @@ Catch {
 Finally {
     Write-Host "Delete of Robintris.wasm finished. Building new one."
 }
+
+Try {
+	del RobintrisARM64.exe
+} 
+Catch {
+	Write-Host "An error occurred del ARM64"
+}
+Finally {
+    Write-Host "Delete of RobintrisARM64.exe finished. Building new one."
+}
+
+$Env:GOARCH = 'arm64'
+go build -o RobintrisARM64.exe -tags=ebitenginesinglethread main.go
+Remove-Item Env:GOARCH
+
 $Env:GOOS = 'js'
 $Env:GOARCH = 'wasm'
 go build -o Robintris.wasm -tags=ebitenginesinglethread -ldflags="-s -w" main.go
